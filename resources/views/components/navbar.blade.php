7ead4ea802e1918bc45f189879617d17095f4cba
AlessandroAmen <xela9827@gmail.com>
2025-04-26 17:05:09 +0200

    miglioramente navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,17 +1,24 @@
 <nav class="navbar">
     <ul>
-        <li><a href="{{ route('homepage') }}">Homepage</a></li>
-        @guest
-            <li><a href="{{ route('register.form') }}">Registrazione</a></li>
-            <li><a href="{{ route('login.form') }}">Login</a></li>
-        @else
-            <li><a href="{{ route('profile', ['id' => Auth::id()]) }}">Profilo</a></li>
-            <li>
-                <form action="{{ route('logout') }}" method="POST" style="display: inline;">
-                    @csrf
-                    <button type="submit" class="btn-logout">Logout</button>
-                </form>
-            </li>
-        @endguest
+        <div class="nav-links">
+            <li><a href="{{ route('homepage') }}">Homepage</a></li>
+            @auth
+                <li><a href="{{ route('profile', ['id' => Auth::id()]) }}">Profilo</a></li>
+            @endauth
+        </div>
+        
+        <div class="auth-links">
+            @guest
+                <li><a href="{{ route('register.form') }}">Registrazione</a></li>
+                <li><a href="{{ route('login.form') }}">Login</a></li>
+            @else
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button type="submit" class="btn-logout">Logout</button>
+                    </form>
+                </li>
+            @endguest
+        </div>
     </ul>
 </nav>
